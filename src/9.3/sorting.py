a3c0488a6dd0f9315e1b49414ed04a994ee007c6
Josh Ziebarth <joshua.ziebarth@us.af.mil>
2024-05-06 09:00:10 -0500

    working

--- src/9.3/sorting.py
+++ src/9.3/sorting.py
@@ -9,22 +9,35 @@ Objectives
 - [ ] Heap sort
 - [ ] Quick sort
 """
+import random
+import timeit
+from typing import List
 
-def merge_sort(input: list[int]) -> list[int]:
+SAMPLE_SIZE = 1000
+MIN_RANDOM = -1000
+MAX_RANDOM =  1000
+
+def merge_sort(input: List[int]) -> List[int]:
+    """"""
+    pass
+
+def selection_sort(input: List[int]) -> List[int]:
     """"""
 
-def selection_sort(input: list[int]) -> list[int]:
+def insertion_sort(input: List[int]) -> List[int]:
     """"""
 
-def insertion_sort(input: list[int]) -> list[int]:
+def hashing(input: List[int]) -> List[int]:
     """"""
 
-def hashing(input: list[int]) -> list[int]:
+def heap_sort(input: List[int]) -> List[int]:
     """"""
 
-def heap_sort(input: list[int]) -> list[int]:
+def quick_sort(input: List[int]) -> List[int]:
     """"""
 
-def quick_sort(input: list[int]) -> list[int]:
-    """"""
+if __name__ == '__main__':
+    rand_list = random.sample(range(MIN_RANDOM, MAX_RANDOM), SAMPLE_SIZE)
+    print(rand_list)
 
+    timeit.timeit('merge_sort()', number=1000, globals=)
