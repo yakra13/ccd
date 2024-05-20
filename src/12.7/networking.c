7766f20e5863f6e72a5a9eed73cf3ac99d61c8cc
Josh Ziebarth <joshziebarth@yahoo.com>
2024-05-19 20:34:19 -0500

    commit

---
+++ src/12.7/networking.c
@@ -0,0 +1,22 @@
+/*
+12.7 In C, demonstrate skill in using networking commands accounting for endianness
+Objectives
+
+- [ ] struct sockaddr_un
+- [ ] struct sockaddr_storage
+- [12.8] socket()
+- [ ] gethostname()
+- [12.8] send()
+- [12.8] recv()
+- [ ] sendto()
+- [ ] recvfrom()
+- [12.8] bind()
+- [12.8] connect()
+- [12.8] accept()
+- [ ] getsockopt()
+- [ ] setsockopt()
+- [ ] getaddrinfo()
+- [12.8] struct sockaddr
+- [12.8] struct sockaddr_in
+*/
+#include 
